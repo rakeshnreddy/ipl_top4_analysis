2a489c51184a2bd3c9738fe5b9360d03f3f855fa
rakeshnreddy <rakesh.n99@live.com>
2024-05-07 11:46:10 -0700

    Update ipl_analysis_app.py
    updated game

--- ipl_analysis_app.py
+++ ipl_analysis_app.py
@@ -20,19 +20,18 @@ team_full_names = {
 # Define the current standings and remaining fixtures
 current_standings = {
     'Kolkata': {'Matches': 11, 'Wins': 8, 'Points': 16},
-    'Rajasthan': {'Matches': 10, 'Wins': 8, 'Points': 16},
+    'Rajasthan': {'Matches': 11, 'Wins': 8, 'Points': 16},
     'Chennai': {'Matches': 11, 'Wins': 6, 'Points': 12},
     'Hyderabad': {'Matches': 11, 'Wins': 6, 'Points': 12},
+    'Delhi': {'Matches': 12, 'Wins': 6, 'Points': 12},
     'Lucknow': {'Matches': 11, 'Wins': 6, 'Points': 12},
-    'Delhi': {'Matches': 11, 'Wins': 5, 'Points': 10},
     'Bangalore': {'Matches': 11, 'Wins': 4, 'Points': 8},
     'Punjab': {'Matches': 11, 'Wins': 4, 'Points': 8},
     'Mumbai': {'Matches': 12, 'Wins': 3, 'Points': 8},
     'Gujarat': {'Matches': 11, 'Wins': 4, 'Points': 8}
 }
 
-remaining_fixtures = [
-    ('Delhi', 'Rajasthan'), 
+remaining_fixtures = [ 
     ('Hyderabad', 'Lucknow'), ('Punjab', 'Bangalore'), ('Gujarat', 'Chennai'), 
     ('Kolkata', 'Mumbai'), ('Chennai', 'Rajasthan'), ('Bangalore', 'Delhi'), 
     ('Gujarat', 'Kolkata'), ('Delhi', 'Lucknow'), ('Rajasthan', 'Punjab'), 
