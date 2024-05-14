11f84f7e62058ad8209ddb65f9e16b51ac5ffe48
rakeshnreddy <rakesh.n99@live.com>
2024-05-14 11:45:38 -0700

    Update ipl_analysis_app.py
    Dc vs LSG result

--- ipl_analysis_app.py
+++ ipl_analysis_app.py
@@ -23,16 +23,16 @@ current_standings = {
     'Rajasthan': {'Matches': 12, 'Wins': 8, 'Points': 16},
     'Chennai': {'Matches': 13, 'Wins': 7, 'Points': 14},
     'Hyderabad': {'Matches': 12, 'Wins': 7, 'Points': 14},
+    'Delhi': {'Matches': 14, 'Wins': 7, 'Points': 14},
     'Bangalore': {'Matches': 13, 'Wins': 6, 'Points': 12},
-    'Delhi': {'Matches': 13, 'Wins': 6, 'Points': 12},
-    'Lucknow': {'Matches': 12, 'Wins': 6, 'Points': 12},
+    'Lucknow': {'Matches': 13, 'Wins': 6, 'Points': 12},
     'Gujarat': {'Matches': 13, 'Wins': 5, 'Points': 10},
     'Mumbai': {'Matches': 13, 'Wins': 4, 'Points': 8},
     'Punjab': {'Matches': 12, 'Wins': 4, 'Points': 8}
 }
 
 remaining_fixtures = [ 
-    ('Delhi', 'Lucknow'), ('Rajasthan', 'Punjab'), 
+    ('Rajasthan', 'Punjab'), 
     ('Hyderabad', 'Gujarat'), ('Mumbai', 'Lucknow'), ('Bangalore', 'Chennai'), 
     ('Hyderabad', 'Punjab'), ('Rajasthan', 'Kolkata')
 ]
